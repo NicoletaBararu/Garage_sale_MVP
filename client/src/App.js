f6545b78b7271d611a26f0dd84166d0852fe46b8
NicoletaBararu <nicoletabararu12@gmail.com>
2022-11-14 00:49:43 +0900

    feat: initialized react-app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Garage Sale</h1>
+    </div>
+  );
+}
+
+export default App;
